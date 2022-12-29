d3c1383dff4469f8fe69b4157737bbd1c09a90fb
Maximilian Dietel <77168125+MaximilianDietel03@users.noreply.github.com>
2022-12-29 13:48:20 +0100

    Error handling on Generate Function

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,15 +13,21 @@ Question:
 `;
 const generateAction = async (req, res) => {
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
-  });
-  
+  let baseCompletion;
+  try {
+    baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${req.body.userInput}`,
+      temperature: 0.7,
+      max_tokens: 250,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+    return;
+  }
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
